495b6b098d0ade2f845efc5e7305fef73483d60c
heyong <macx@macx.cn>
2019-08-30 16:06:13 +0800

    修正了gitignore

---
+++ dist/uiserver.js
@@ -0,0 +1,24 @@
+// 引入依赖
+var program = require('commander');
+
+// 命令行模式直接执行脚本
+const Runimage = require('./runimage.js');
+
+const Server = require('./server.js');
+
+// 定义版本和参数选项
+program.version('0.1.0', '-v, --version').option('-i, --initui', 'init uiserver').option('-r, --run', '直接执行脚本');
+
+program.parse(process.argv);
+
+if (program.initui) {
+  console.log('初始化一个容易操作的ui交互');
+  const a = new Server();
+}
+
+if (program.run || program) {
+  if (!program.initui) {
+    const b = new Runimage();
+    b.init(); // 初始化
+  }
+}
